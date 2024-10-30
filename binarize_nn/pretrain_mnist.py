401f1fbd2da434253eb04d46dd3c461b38e7f0ad
mauricett <mauricetpunkt@gmail.com>
2024-10-31 00:33:21 +0100

    pretrain mnist

--- binarize_nn/pretrain_mnist.py
+++ binarize_nn/pretrain_mnist.py
@@ -2,33 +2,42 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
+import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-
-n_layers = 24
+n_layers = 2
 d_model = 1024
-batch_size = 1024
+batch_size = 512
+n_epochs = 25
 
-mnist_args = {
-    'root': 'data',
-    'train': True,
-    'download': False
-}
 
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.1307,), (0.3015,))
 ])
 
-train_data = datasets.MNIST(**mnist_args, transform=transform)
-train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+train_data = datasets.MNIST(root='data', transform=transform, train=True)
+val_data = datasets.MNIST(root='data', transform=transform, train=False)
+
+def accuracy(model, test_data):
+    val_loader = DataLoader(test_data, batch_size, shuffle=True)
+    n_total = test_data.__len__()
+    n_correct = 0
+
+    with torch.no_grad():
+        for x, label in val_loader:
+            out = model(x)
+            prediction = torch.argmax(out, dim=-1)
+            n_correct += torch.sum(label == prediction)
+
+    return n_correct / n_total * 100.
 
 
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.input_layer  = nn.Linear(28*28, d_model)
+        self.input_layer = nn.Linear(28*28, d_model)
         self.output_layer = nn.Linear(d_model, 10)
         self.hidden_layers = [
             (nn.LayerNorm(d_model), nn.Linear(d_model, d_model))
@@ -36,19 +45,37 @@ class Model(nn.Module):
         ]
 
     def forward(self, x):
-        x = self.input_layer(x.reshape(batch_size, -1))
+        bs, _, _, _ = x.shape
+        x = self.input_layer(x.reshape(bs, -1))
         for layernorm, linear in self.hidden_layers:
             x = layernorm(F.gelu(x))
             x = linear(x)
         return self.output_layer(F.gelu(x))
 
 
-x, y = next(iter(train_loader))
 model = Model()
+#model = torch.compile(model)
+
 loss_fn = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=5e-4)
 
-#%%
-with torch.no_grad():
-    out = model(x)
+for n in range(n_epochs):
+    print("Epoch: ", n)
+    train_loader = DataLoader(train_data, batch_size, shuffle=True)
 
-loss = loss_fn(out, y)
+    for x, label in train_loader:
+        out = model(x)
+        loss = loss_fn(out, label)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    acc = accuracy(model, val_data)
+    print(acc)
+
+print("Training done :)")
+
+torch.save(model.state_dict(), './model/2_layer_mlp.pt')
+torch.save(optimizer.state_dict(), './model/2_layer_mlp_adam.pt')
+
+print("Model saved ;)")
